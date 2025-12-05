db7459c7a5eba7733b6fb66d242d3bd2977ce41e
EJStuckey <edward.stuckey@physics.uu.se>
2025-12-05 09:55:19 +0100

    flake8 formatting

--- hogben/models/base.py
+++ hogben/models/base.py
@@ -318,12 +318,13 @@ class BaseLipid(BaseSample, VariableContrast, VariableUnderlayer):
 
         # Plot the SLD profile for each measured contrast.
         for structure in self.structures:
-            #Limits for the SLD profile plotting based on total slab extent and roughnesses
-            zmin = 0-4*int(structure.slabs()[1, 3])-10
-            zmax = structure.slabs()[:,0].sum()+4*int(structure.slabs()[-1, 3])+10
+            # Limits for SLD plotting based on thickness and roughnesses
+            zmin = 0 - 4 * int(structure.slabs()[1, 3]) - 10
+            zmax = structure.slabs()[:, 0].sum()
+            + 4 * int(structure.slabs()[-1, 3]) + 10
             zsteps = 500
-            ax.plot(*structure.sld_profile(np.linspace(zmin,zmax,zsteps)))
-            
+            ax.plot(*structure.sld_profile(np.linspace(zmin, zmax, zsteps)))
+
         x_label = '$\mathregular{Distance\ (\AA)}$'
         y_label = '$\mathregular{SLD\ (10^{-6} \AA^{-2})}$'
 
